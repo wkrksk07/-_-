4d3ed4be84785da753392a2a0cd43888ce055be9
wkrksk07 <040306@naver.com>
2022-09-15 15:38:58 +0900

    123132132

--- 미연시/코딩_이수,인욱/Game_aFly/MainGame.py
+++ 미연시/코딩_이수,인욱/Game_aFly/MainGame.py
@@ -3,7 +3,9 @@ __author__ = 'hahaha'
 import pygame as py
 import random
 ## sleep = 잠깐 멈춤
-from time import sleep
+from time import sleep, time
+
+py.init()
 
 WHITE = (255,255,255)
 RED = (255,0,0)
@@ -66,11 +68,29 @@ def drawObject(obj, x, y):
     global gamepad
     gamepad.blit(obj,(x, y))
 
+class Button:
+    def __init__(self, img_in, x, y, width, height, img_act, x_act, y_act, action = None):
+        global gamepad, startStr
+
+        mouse = py.mouse.get_pos() ##마우스 좌표 저장
+        click = py.mouse.get_pressed() ##클래식
+
+        if x + width > mouse[0] > x and y + height > mouse[1] > y: ##이미지 안에 있다면
+            gamepad.blit(img_act,(x_act,y_act)) ##클릭이미지 로드
+            if click[0]: ## and action != None
+                sleep(1) ## 1초 지연
+                ##action() ##지정 함수 호출
+                startStr = "STAGE_1"
+                stageChainge()
+            else:
+                gamepad.blit(img_in,(x,y)) ##마우스 이미지 바깥이면 일반 이미지 로드
+
 def runGame():
     global gamepad, aircraft, clock, background1, background2
-    global bat, fires, bullet, boom, shot_sound
+    global bat, fires, bullet, boom, shot_sound, startStr, StartImg, ClickStartImg
 
     Player_HP = 100
+    startStr = "MAIN"
 
     isShotBat = False
     boom_count = 0
@@ -96,137 +116,153 @@ def runGame():
 
     crashed = False
     while not crashed:
-        for event in py.event.get():
-            if event.type == py.QUIT:
-                crashed = True
+        if startStr == "MAIN":
+            for event in py.event.get():
+                if event.type == py.QUIT:
+                    crashed = True
+            #Clear gamepad
+            gamepad.fill(WHITE)
 
-            if event.type == py.KEYDOWN:
-                if event.key == py.K_UP:
-                    y_change = -5
+            startButton = Button(StartImg,280,260,60,20,ClickStartImg,273,258,None)
+            
 
-                elif event.key == py.K_DOWN:
-                    y_change = 5
-                
-                elif event.key == py.K_LCTRL:
-                    bullet_x = x + aircraft_width
-                    bullet_y = y + aircraft_height/2
-                    bullet_xy.append([bullet_x, bullet_y])
-                    ##py.mixer.Sound.play(shot_sound)
+        elif startStr == "STAGE_1":
+            for event in py.event.get():
+                if event.type == py.QUIT:
+                    crashed = True
+            
+                if event.type == py.KEYDOWN:
+                    if event.key == py.K_UP:
+                        y_change = -5
 
-            if event.type == py.KEYUP:
-                if event.key == py.K_UP or event.key == py.K_DOWN:
-                    y_change = 0
-
-        #Clear gamepad
-        gamepad.fill(WHITE)
-        
-        #Draw Background
-        background1_x -= 2
-        background2_x -= 2
-
-        if background1_x == -background_width:
-            background1_x = background_width
-
-        if background2_x == -background_width:
-            background2_x = background_width
-
-        drawObject(background1,background1_x,0)
-        drawObject(background2,background2_x,0)
-
-        drawScore(bat_passed, Player_HP)
-
-        #Check the number of BAT passed
-        if bat_passed > 5:
-            gameComplte()
-
-        #Aircraft Postion
-        y += y_change
-        if y<0:
-            y = 0
-        elif y > pad_height - aircraft_height:
-            y = pad_height - aircraft_height
-
-        #Bat Pos
-        bat_x -= 7
-        if bat_x <= 0:
-            bat_x = pad_width
-            bat_y = random.randrange(0,pad_height)
-
-        #Fireball Pos
-        if fire[1] == None:
-            fire_x -= 30
-        else:
-            fire_x -= 15
-
-        if fire_x <= 0:
-            fire_x = pad_width
-            fire_y = random.randrange(0,pad_height)
-            random.shuffle(fires)
-            fire = fires[0]
-
-        #Bullet pos
-        if len(bullet_xy) != 0:
-            for i, bxy in enumerate(bullet_xy):
-                bxy[0] += 15
-                bullet_xy[i][0] = bxy[0]
-
-                #Check if bullet strike Bat
-                if bxy[0] > bat_x:
-                    if bxy[1] > bat_y and bxy[1] < bat_y + bat_height:
-                        bullet_xy.remove(bxy)
-                        bat_passed += 1 
-                        isShotBat = True
-
-                if bxy[0] >= pad_width:
-                    try:
-                        bullet_xy.remove(bxy)
-                    except:
-                        pass
-
-        #Check aircraft crashed by BAT
-        if x + aircraft_width > bat_x:
-            if(y > bat_y and y < bat_y + bat_height) or\
-            (y+aircraft_height > bat_y and y + aircraft_height < bat_y + bat_height):
-                Player_HP += 1
-                ##crash()
-
-        #Check aircraft crashed by Fireball
-        if fire[1] != None:
-            if fire[0] == 0:
-                fireball_width = fireball1_width
-                fireball_height = fireball1_height
-            elif fire[0] == 1:
-                fireball_width = fireball2_width
-                fireball_height = fireball2_height
-
-            if x+ aircraft_width > fire_x:
-                if(y>fire_y and y < fire_y + fireball_height) or\
-                (y+ aircraft_height > fire_y and y + aircraft_height < fire_y + fireball_height):
-                    crash()
+                    elif event.key == py.K_DOWN:
+                        y_change = 5
                     
+                    elif event.key == py.K_LCTRL:
+                        bullet_x = x + aircraft_width
+                        bullet_y = y + aircraft_height/2
+                        bullet_xy.append([bullet_x, bullet_y])
+                        ##py.mixer.Sound.play(shot_sound)
 
-        drawObject(aircraft, x,y)
+                if event.type == py.KEYUP:
+                    if event.key == py.K_UP or event.key == py.K_DOWN:
+                        y_change = 0
 
-        if len(bullet_xy) != 0:
-            for bx, by in bullet_xy:
-                drawObject(bullet, bx, by)
+            #Clear gamepad
+            gamepad.fill(WHITE)
+            
+            #Draw Background
+            background1_x -= 2
+            background2_x -= 2
 
-        if not isShotBat:
-            drawObject(bat,bat_x,bat_y)
-        else:
-            drawObject(boom,bat_x,bat_y)
-            boom_count +=1
-            if boom_count > 5:
-                boom_count = 0
+            if background1_x == -background_width:
+                background1_x = background_width
+
+            if background2_x == -background_width:
+                background2_x = background_width
+
+            drawObject(background1,background1_x,0)
+            drawObject(background2,background2_x,0)
+
+            drawScore(bat_passed, Player_HP)
+
+            #Check the number of BAT passed
+            if bat_passed > 5:
+                gameComplte()
+
+            #Aircraft Postion
+            y += y_change
+            if y<0:
+                y = 0
+            elif y > pad_height - aircraft_height:
+                y = pad_height - aircraft_height
+
+            #Bat Pos
+            bat_x -= 7
+            if bat_x <= 0:
                 bat_x = pad_width
-                bat_y = random.randrange(0, pad_height-bat_height)
-                isShotBat = False
+                bat_y = random.randrange(0,pad_height)
 
-        if fire[1] != None:
-            drawObject(fire[1], fire_x, fire_y)
+            #Fireball Pos
+            if fire[1] == None:
+                fire_x -= 30
+            else:
+                fire_x -= 15
+
+            if fire_x <= 0:
+                fire_x = pad_width
+                fire_y = random.randrange(0,pad_height)
+                random.shuffle(fires)
+                fire = fires[0]
+
+            #Bullet pos
+            if len(bullet_xy) != 0:
+                for i, bxy in enumerate(bullet_xy):
+                    bxy[0] += 15
+                    bullet_xy[i][0] = bxy[0]
+
+                    #Check if bullet strike Bat
+                    if bxy[0] > bat_x:
+                        if bxy[1] > bat_y and bxy[1] < bat_y + bat_height:
+                            bullet_xy.remove(bxy)
+                            bat_passed += 1 
+                            isShotBat = True
+
+                    if bxy[0] >= pad_width:
+                        try:
+                            bullet_xy.remove(bxy)
+                        except:
+                            pass
+
+            #Check aircraft crashed by BAT
+            if x + aircraft_width > bat_x:
+                if(y > bat_y and y < bat_y + bat_height) or\
+                (y+aircraft_height > bat_y and y + aircraft_height < bat_y + bat_height):
+                    Player_HP += 1
+                    ##crash()
+
+            #Check aircraft crashed by Fireball
+            if fire[1] != None:
+                if fire[0] == 0:
+                    fireball_width = fireball1_width
+                    fireball_height = fireball1_height
+                elif fire[0] == 1:
+                    fireball_width = fireball2_width
+                    fireball_height = fireball2_height
+
+                if x+ aircraft_width > fire_x:
+                    if(y>fire_y and y < fire_y + fireball_height) or\
+                    (y+ aircraft_height > fire_y and y + aircraft_height < fire_y + fireball_height):
+                        crash()
+                        
+
+            drawObject(aircraft, x,y)
+
+            if len(bullet_xy) != 0:
+                for bx, by in bullet_xy:
+                    drawObject(bullet, bx, by)
+
+            if not isShotBat:
+                drawObject(bat,bat_x,bat_y)
+            else:
+                drawObject(boom,bat_x,bat_y)
+                boom_count +=1
+                if boom_count > 5:
+                    boom_count = 0
+                    bat_x = pad_width
+                    bat_y = random.randrange(0, pad_height-bat_height)
+                    isShotBat = False
+
+            if fire[1] != None:
+                drawObject(fire[1], fire_x, fire_y)
+
+            
+        ## 버튼 눌렀을 때 작동되게 if stageChainge()
 
         py.display.update()
         clock.tick(60)
+
+
 
     py.quit()
     quit()
@@ -234,7 +270,7 @@ def runGame():
 def initGame():
     global gamepad, aircraft, clock, background1, background2
     global bat, fires, bullet, boom
-    global shot_sound, explosion_sound
+    global shot_sound, explosion_sound, StartImg, ClickStartImg
 
     ## 배경음
     ##py.mixer.music.load('주소Game_aFly/Audio/mybgm.wav')
@@ -251,6 +287,9 @@ def initGame():
 
     background1 = py.image.load('Game_aFly/Img/background.png')
     background2 = background1.copy()
+
+    StartImg = py.image.load('Game_aFly/Img/clickedStartIcon.png')
+    ClickStartImg = StartImg.copy()
 
     bat = py.image.load('Game_aFly/Img/bat.png')
 
@@ -270,9 +309,76 @@ def initGame():
     clock = py.time.Clock()
     runGame()
 
+def stageChainge():
+    global gamepad, aircraft, clock, background1, background2
+    global bat, fires, bullet, boom
+    global shot_sound, explosion_sound, startStr
+
+    if startStr == "MAIN":
+        ## 배경음
+        ##py.mixer.music.load('주소Game_aFly/Audio/mybgm.wav')
+        ##py.mixer.music.play(-1)
+
+        fires = []
+
+        py.init()
+        gamepad = py.display.set_mode((pad_width, pad_height))
+        py.display.set_caption('Sleve_To_Coding')
+
+        aircraft = py.image.load('Game_aFly/Img/plane.png')
+
+        background1 = py.image.load('Game_aFly/Img/background.png')
+        background2 = background1.copy()
+
+        bat = py.image.load('Game_aFly/Img/bat.png')
+
+        fires.append((0, py.image.load('Game_aFly/Img/fireball.png')))
+        fires.append((1, py.image.load('Game_aFly/Img/fireball2.png')))
+
+        boom = py.image.load('Game_aFly/Img/boom.png')
+
+        for i in range(3):
+            fires.append((i+2, None))
+
+        bullet = py.image.load('Game_aFly/Img/bullet.png')
+
+        ##shot_sound = py.mixer.Sound('Game_aFly/Audio/shot.wav')
+        ##explosion_sound = py.mixer.Sound('Game_aFly/Audio/explosion.wav')
+
+    elif startStr == "STAGE":
+        ## 배경음
+        ##py.mixer.music.load('주소Game_aFly/Audio/mybgm.wav')
+        ##py.mixer.music.play(-1)
+
+        fires = []
+
+        py.init()
+        gamepad = py.display.set_mode((pad_width, pad_height))
+        py.display.set_caption('Sleve_To_Coding')
+
+        aircraft = py.image.load('Game_aFly/Img/plane.png')
+
+        background1 = py.image.load('Game_aFly/Img/background.png')
+        background2 = background1.copy()
+
+        bat = py.image.load('Game_aFly/Img/bat.png')
+
+        fires.append((0, py.image.load('Game_aFly/Img/fireball.png')))
+        fires.append((1, py.image.load('Game_aFly/Img/fireball2.png')))
+
+        boom = py.image.load('Game_aFly/Img/boom.png')
+
+        for i in range(3):
+            fires.append((i+2, None))
+
+        bullet = py.image.load('Game_aFly/Img/bullet.png')
+
+        ##shot_sound = py.mixer.Sound('Game_aFly/Audio/shot.wav')
+        ##explosion_sound = py.mixer.Sound('Game_aFly/Audio/explosion.wav')
+
 
 if __name__ == '__main__':
     initGame()
 
 
-## https://m.blog.naver.com/samsjang/220713309790 
+## https://m.blog.naver.com/samsjang/220713309790 https://www.pygame.org/tags/game  https://www.google.com/search?sxsrf=ALiCzsYSKMLDGRVA-45hotIm6za1QiqctQ:1663053970738&q=Pygame+%ED%99%94%EB%A9%B4+%EC%A0%84%ED%99%98&sa=X&ved=2ahUKEwjN56vpnpH6AhXUad4KHfgPANcQ1QJ6BAgsEAE&biw=1920&bih=948&dpr=1
